6dfa5670d3f9b84e5752f83205f81568631cd15c
Arnaud Lenglet <arlenglet@gmail.com>
2015-09-10 09:39:17 +0200

    feat(migration): migrate db

--- db/migrate/20150910072306_create_cloths.rb
+++ db/migrate/20150910072306_create_cloths.rb
@@ -1,15 +1,16 @@
 class CreateCloths < ActiveRecord::Migration
   def change
     create_table :cloths do |t|
-      t.references :dressing, index: true
       t.string :name
-      t.references :category, index: true
+      t.references :dressing, index: true
+      t.references :cloth_category, index: true
       t.references :color, index: true
 
       t.timestamps null: false
     end
+
     add_foreign_key :cloths, :dressings
-    add_foreign_key :cloths, :categories
+    add_foreign_key :cloths, :cloth_categories
     add_foreign_key :cloths, :colors
   end
 end
